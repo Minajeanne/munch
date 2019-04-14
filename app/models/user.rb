1fba299eae5b4814fe8c701d8868e2121ba04261
Minajeanne <albrown07@mac.com>
2019-04-14 15:36:40 -0400

    added uniqueness to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :username, :email, :password, :full_name
+  validates_presence_of :password, :full_name
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   has_many :reviews
   has_many :restaurants, through: :reviews
