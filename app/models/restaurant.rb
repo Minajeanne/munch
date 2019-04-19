b065a9c79b424be798ecb02a6a90fc73ac92425b
Minajeanne <albrown07@mac.com>
2019-04-19 09:38:11 -0400

    added notes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
   validates_presence_of :cuisine, :location #:overall_rating
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true #validates restaurant name to ensure uniqueness/prevent same restaurant from being persisted to database more than once.
 
   has_many :reviews
   has_many :users, through: :reviews
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20190410161116) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password_digest" #required for has_secure_password
     t.string "full_name"
   end
 
